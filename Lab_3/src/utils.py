b52e5a5c5f4c84f6c211af928658348f83efb90b
Daniel <pini.dpm@gmail.com>
2025-10-19 18:34:24 +0200

    Update lab 3

--- Lab_3/src/utils.py
+++ Lab_3/src/utils.py
@@ -6,6 +6,43 @@ from bow import BoW
 from typing import List
 from image_classifier import ImageClassifier
 
+def non_max_suppression(img, theta):
+
+    M, N = img.shape
+    Z = np.zeros((M, N), dtype=np.float32)
+
+    # converting radians to degree
+    angle = theta * 180. / np.pi    # max -> 180, min -> -180
+    angle[angle < 0] += 180         # max -> 180, min -> 0
+
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            q = 255
+            r = 255
+
+            # angle 0
+            if (0 <= angle[i, j] < 22.5) or (157.5 <= angle[i, j] <= 180):
+                r = img[i, j-1]
+                q = img[i, j+1]
+            # angle 45
+            elif (22.5 <= angle[i, j] < 67.5):
+                r = img[i-1, j+1]
+                q = img[i+1, j-1]
+            # angle 90
+            elif (67.5 <= angle[i, j] < 112.5):
+                r = img[i-1, j]
+                q = img[i+1, j]
+            # angle 135
+            elif (112.5 <= angle[i, j] < 157.5):
+                r = img[i+1, j+1]
+                q = img[i-1, j-1]
+
+            if (img[i, j] >= q) and (img[i, j] >= r):
+                Z[i, j] = img[i, j]
+            else:
+                Z[i, j] = 0
+    return Z
+
 def visualizeKp(image, keypoints):
     output_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     output_image = output_image.astype(np.uint8)
